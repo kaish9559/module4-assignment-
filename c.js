4b43dc9d680c7cbb868768191c0592226bffd36e
kaish9559 <67217018+kaish9559@users.noreply.github.com>
2020-07-20 12:00:11 +0530

    Create c.js

---
+++ c.js
@@ -0,0 +1,14 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+
+console.log(names[i]);
+
+
+var firstLetter = names[i].charAt(0).toLowerCase();
+if (firstLetter === 'j') {
+    console.log("byeSpeaker"+names[i]);
+  } else {
+    console.log("hello"+names[i]);
+  }
+}
