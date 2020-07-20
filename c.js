b3e392971296acf70213e79848b8ac508736c7d1
kaish9559 <67217018+kaish9559@users.noreply.github.com>
2020-07-20 12:25:03 +0530

    Update c.js

--- c.js
+++ c.js
@@ -7,7 +7,7 @@ console.log(names[i]);
 
 var firstLetter = names[i].charAt(0).toLowerCase();
 if (firstLetter === 'j') {
-    console.log("byeSpeaker"+names[i]);
+    console.log("Goodbye"+names[i]);
   } else {
     console.log("hello"+names[i]);
   }
